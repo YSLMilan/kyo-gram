14fdeef06c0aca299ce7218a0184f8dcdc9b4db3
Bubs <90681784+YSLMilan@users.noreply.github.com>
2024-10-11 17:28:12 +0100

    Update firebase-config.js

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbAPPMSp_JY0L4dnL69lBXNFzFXRYfNBw",
-  authDomain: "kiyo-gram.firebaseapp.com",
-  projectId: "kiyo-gram",
-  storageBucket: "kiyo-gram.appspot.com",
-  messagingSenderId: "90922899745",
-  appId: "1:90922899745:web:8cd10c0bdc3158942c4660",
-  measurementId: "G-6Z7M4F2WLV",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
